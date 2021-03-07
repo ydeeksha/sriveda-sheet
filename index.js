ce570c5579c50f31a20cd80f4979ca9fdc08fbf0
ydeeksha <75473851+ydeeksha@users.noreply.github.com>
2021-03-07 17:34:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("my first console project");
+var first name = "rabin";
+var last name ="kapoor";
+var full name = 
+first name.concat ("last name");
+console.log(full name);
+var a = 10;
+var b = 5;
+var add = a + b ;
+console.log (add);
+var add = a - b ;
+console.log (sub);
+var add = a * b ;
+console.log (multiple);
+var add = a  b ;
+console.log (divide);
